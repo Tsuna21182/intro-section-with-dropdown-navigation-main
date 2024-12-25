169f49e281ab2ec94002fa8e2b353cab71c2c83f
Tsuna21182 <tsunadelanochecer@gmail.com>
2024-12-24 20:01:45 -0500

    integracion boton menu

--- js/main.js
+++ js/main.js
@@ -1,9 +1,72 @@
 const menu = document.querySelector(".menu-icon");
+const nav = document.querySelector(".header__nav");
+const navDropdownBtn = document.querySelector(".nav__dropdown .dropdown__btn");
+const dropdownBtn = document.querySelector(".dropdown .dropdown__btn");
+const dropContent = document.querySelector(".dropdown__content");
+const dropContent2 = document.querySelector(".dropdown__content-2");
+const Features = document.querySelector(".features-btn");
+const Company = document.querySelector(".company-btn");
 
+// Manejar el menú móvil
 menu.addEventListener("click", () => {
-  if (menu.classList.contains("menu-icon-activo")) {
-    menu.classList.remove("menu-icon-activo");
+  menu.classList.toggle("menu-icon-activo");
+  nav.classList.toggle("header__nav-active");
+  if (dropdownBtn.classList.contains("active")) {
+    dropdownBtn.classList.remove("active");
+    navDropdownBtn.classList.remove("active");
   } else {
-    menu.classList.add("menu-icon-activo");
+    dropdownBtn.classList.add("active");
+    navDropdownBtn.classList.add("active");
   }
+});
+
+// Manejar el dropdown "Features"
+navDropdownBtn.addEventListener("click", (event) => {
+  event.stopPropagation();
+  dropContent.classList.toggle("active");
+});
+
+// Manejar el dropdown "Company"
+dropdownBtn.addEventListener("click", (event) => {
+  event.stopPropagation();
+  dropContent2.classList.toggle("active");
+});
+
+// Cerrar dropdowns al hacer clic fuera
+document.addEventListener("click", (event) => {
+  const isClickInsideMenu =
+    menu.contains(event.target) ||
+    nav.contains(event.target) ||
+    Features.contains(event.target) ||
+    Company.contains(event.target) ||
+    dropContent.contains(event.target) ||
+    dropContent2.contains(event.target) ||
+    navDropdownBtn.contains(event.target) ||
+    dropdownBtn.contains(event.target);
+
+  if (!isClickInsideMenu) {
+    // Cerrar menú móvil si está activo
+    menu.classList.remove("menu-icon-activo");
+    nav.classList.remove("header__nav-active");
+
+    // Cerrar dropdowns si están activos
+    dropContent.classList.remove("active");
+    dropContent2.classList.remove("active");
+  }
+});
+
+Features.addEventListener("click", (event) => {
+  event.stopPropagation();
+  if (Features.classList.contains("active")) {
+    dropContent.classList.remove("active");
+    Features.classList.remove("active");
+  } else {
+    dropContent.classList.add("active");
+    Features.classList.add("active");
+  }
+});
+
+Company.addEventListener("click", (event) => {
+  event.stopPropagation();
+  Company.classList.add("active");
 });
